f705a026e12ab58a06fa14065f7216f958aaf53a
Rishikesh_Kumar <rishikeshkumar0804@gmail.com>
2024-01-02 20:44:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg'; 
+import SortingVisualizer from './SortingVisualizer/SortingVisualizer';
+import './App.css';
+// import SideBar from './Components/SideBar';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SideBar></SideBar> */}
+      <SortingVisualizer></SortingVisualizer>
+    </div>
+  );
+}
+
+export default App;
